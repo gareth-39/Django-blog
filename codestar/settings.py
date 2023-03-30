3870f29155af87947c59eefd2e43bd8597b29947
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-03-30 14:22:32 +0000

    deployment 2

--- codestar/settings.py
+++ codestar/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'cloudinary_storage',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'blog',
